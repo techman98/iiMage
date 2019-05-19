0539f505f725351115a16030f2fd50533353d0f8
Techman <wizmar.tyler@gmail.com>
2019-05-19 18:53:32 -0500

    Profile page looks decent, having trouble getting new images added when there is no more space on a row.

--- client/src/pages/Creator/categories.js
+++ client/src/pages/Creator/categories.js
@@ -1,8 +1,34 @@
 const categories = [{
     "image": "http://www.dj2legit.com/uploads/1/0/0/9/100962028/couple_orig.jpg",
-    "category" : "Wedding"
+    "category" : "Weddings"
 },
 {
-    "category" : "Football Games"
-}]
+    "category" : "Football Games",
+    "image" : "http://www.frugaa.com/images/american-football-game.jpg"
+},
+{
+    "category": "Babyshowers",
+    "image" : "https://tse3.mm.bing.net/th?id=OIP.-6Y_vKU5zWghPRR95eIEBwHaHa&pid=Api&P=0&w=300&h=300"
+},
+{
+    "category" : "Graduations",
+    "image" : "https://tse1.mm.bing.net/th?id=OIP.Zy0Kt5qcJlXm0xVcd2BYDgHaE7&pid=Api&P=0&w=261&h=174"
+},
+{
+    "category": "Music Festival",
+    "image" : "https://i.axs.com/2017/05/31581-image_5908bde383b97.jpg"
+},
+{
+    "category" : "India",
+    "image" : "https://tse2.mm.bing.net/th?id=OIP.QLS7_wMFaUAJkZ25GxDL7wHaE3&pid=Api&P=0&w=272&h=179"
+}
+// {
+//     "category": "Greece",
+//     "image" : "https://tse1.mm.bing.net/th?id=OIP.UKVr7m1J5WL9z5WBfjLyvAHaE8&pid=Api&P=0&w=263&h=176"
+// }
+// {
+//     "category": "Studios",
+//     "image" : "https://tse3.mm.bing.net/th?id=OIP.jN_9ln7HqNGWOqyuGpdmAAHaEx&pid=Api&P=0&w=261&h=169"
+// }
+]
 export default categories;
